9650f98a455595464a11e0db4e2384a98aaedaf8
Joaquin Fernandez <joaquinfernandez@MacBook-Air-de-Joaquin.local>
2025-04-07 14:42:44 -0300

    separo test del archivo funcion

---
+++ test/test_roman_converter.py
@@ -0,0 +1,45 @@
+import unittest
+from scr.roman_converter import roman_to_decimal
+
+class TestRomanToDecimal(unittest.TestCase):
+    def test_I(self):
+        decimal = roman_to_decimal('I')
+        self.assertEqual(decimal, 1)
+
+    def test_II(self):
+        decimal = roman_to_decimal('II')
+        self.assertEqual(decimal, 2)
+
+    def test_III(self):
+        decimal = roman_to_decimal('III')
+        self.assertEqual(decimal, 3)
+
+    def test_V(self):
+        decimal = roman_to_decimal('V')
+        self.assertEqual(decimal, 5)
+
+    def test_VI(self):
+        decimal = roman_to_decimal('VI')
+        self.assertEqual(decimal, 6)
+
+    def test_VIII(self):
+        decimal = roman_to_decimal('VIII')
+        self.assertEqual(decimal, 8)
+
+    def test_IV(self):
+        decimal = roman_to_decimal('IV')
+        self.assertEqual(decimal, 4)
+
+    def test_IX(self):
+        decimal = roman_to_decimal('IX')
+        self.assertEqual(decimal, 9)
+
+def main():
+    my_roman = input('decime tu romano: ')
+    decimal_humano = roman_to_decimal(my_roman)
+
+    print(roman_to_decimal + ' -> ' + str(decimal_humano))
+
+
+if __name__ == '__main__':
+    unittest.main()
